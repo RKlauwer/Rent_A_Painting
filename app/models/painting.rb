2904f6c8ffcc43b9470c6d6d5245e6556e27b603
Raymond Klauwer <klauwerraymond@gmail.com>
2021-11-23 15:27:30 -0600

    add validations to author, size and price

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,3 +1,6 @@
 class Painting < ApplicationRecord
   belongs_to :user
+  validates :author, length: { minimum: 2 }, presence: true
+  validates :size, presence: true
+  validates :price, presence: true
 end
