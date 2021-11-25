96387c6295aab7e747b0543f28424455ddd74c80
Maryan Berhe <maryanberhe@gmail.com>
2021-11-25 17:48:58 -0600

    worked on bookings css and modified app_html as well as some other things

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :paintings do
-    resources :bookings
+    resources :bookings, only: [:index, :new, :create]
   end
+  resources :bookings, except: [:index, :new, :create]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
